e5f441f046d86bdf291e113747acff918eac88cd
phalla.doll <phalla.doll@gmail.com>
2025-12-17 09:09:44 +0700

    Add biome configuration file and update project dependencies; remove ESLint config, adjust package scripts for Biome, and refactor various components for consistency.

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
     return (
         <html lang="en" className={dmSans.variable}>
             <body
-                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+                className={`${geistSans.variable} ${geistMono.variable} dark antialiased`}
             >
                 {children}
             </body>
